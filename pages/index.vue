2eb397d5a26f53c9bd27a0e680edf1e12bed5d40
MarcusCunhaPessoalM1Air <marcus.cunha@engdb.com.br>
2022-03-03 15:02:01 -0400

    install tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,34 @@
 <template>
-  <div class="container">
-    <img class="logo" src="~/assets/oruga.png" />
-    <h1 class="title">Oruga</h1>
-    <p class="subtitle">
-      Oruga UI is like a caterpillar, minimal and yet functional.<br />It's in
-      your hands turning it into a butterfly.
-    </p>
-    <div class="buttons">
-      <o-button
-        root-class="button"
-        variant-class="button-"
-        variant="light"
-        @click="goToDocs()"
+  <div class="p-4">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+      <img
+        class="w-full"
+        src="https://tailwindcss.com/img/card-top.jpg"
+        alt="Sunset in the mountains"
       >
-        Documentation
-      </o-button>
-      <o-button
-        root-class="button"
-        variant-class="button-"
-        variant="dark"
-        @click="goToGithub()"
-      >
-        GitHub
-      </o-button>
+      <div class="px-6 py-4 pb-2">
+        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p
+          class="text-gray-700 text-base"
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+      </div>
+      <div class="px-6 py-4">
+        <span class="badge mr-2">#photography</span>
+        <span class="badge mr-2">#travel</span>
+        <span class="badge">#winter</span>
+      </div>
     </div>
+    <NuxtLink to="about">
+      <TButton>About</TButton>
+    </NuxtLink>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'IndexPage',
-  methods: {
-    goToDocs() {
-      window.location = 'https://oruga.io/documentation/#nuxt-module'
-    },
-    goToGithub() {
-      window.location = 'https://github.com/oruga-ui/oruga'
-    },
-  },
-}
-</script>
-
-<style>
-.logo {
-  max-width: 300px;
-}
-
-.o-button {
-  padding: 10px 30px;
-}
-
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  flex-direction: column;
-}
-
-.title {
-  font-family: Quicksand, 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.buttons {
-  padding-top: 15px;
-}
-
-.button {
-  padding: 1rem;
-  min-width: 10rem;
-}
-
-.button-light {
-  background-color: #9dbe43;
-}
-
-.button-dark {
-  background-color: #293923;
+<style lang="postcss" scoped>
+.badge {
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+  &:hover {
+    @apply bg-gray-300;
+  }
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,331 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          DEFAULT: colors.gray[700],
+          dark: '#ffffff',
+        },
+        primary: {
+          DEFAULT: '#1fb6ff',
+          dark: '#ffffff',
+        },
+        highlight: {
+          DEFAULT: colors.red[700],
+          dark: colors.violet[800],
+        },
+        'highlight-background': {
+          DEFAULT: colors.yellow[400],
+          dark: '#1fb6ff',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
+// "color": {
+//   "white": {
+//     "value": "#fff",
+//     "description": "white",
+//     "type": "color"
+//   },
+//   "grey-25": {
+//     "value": "#F9F9F9",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "grey-50": {
+//     "value": "#f4f4f4",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "grey-100": {
+//     "value": "#EAEAEA",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "grey-200": {
+//     "value": "#BCBCBC",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "grey-300": {
+//     "value": "#6E6E6E",
+//     "description": "grey 300",
+//     "type": "color"
+//   },
+//   "grey-400": {
+//     "value": "#323232",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "black": {
+//     "value": "#000",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "violet-400": {
+//     "value": "#5D2786",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "violet-300": {
+//     "value": "#7933AC",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "violet-200": {
+//     "value": "#8E3CCB",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "violet-100": {
+//     "value": "#B24BFF",
+//     "description": "primary 100",
+//     "type": "color"
+//   },
+//   "orange-400": {
+//     "value": "#C14726",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "orange-300": {
+//     "value": "#E3542D",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "orange-200": {
+//     "value": "#FF5E32",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "orange-100": {
+//     "value": "#FF8C32",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "red-400": {
+//     "value": "#B32E34",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "red-300": {
+//     "value": "#D7373F",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "red-200": {
+//     "value": "#E34850",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "red-100": {
+//     "value": "#FC5059",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "yellow-400": {
+//     "value": "#D6A200",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "yellow-300": {
+//     "value": "#F0B600",
+//     "description": "warning 300",
+//     "type": "color"
+//   },
+//   "yellow-200": {
+//     "value": "#FFC200",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "yellow-100": {
+//     "value": "#FFD54E",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "green-400": {
+//     "value": "#1F7567",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "green-300": {
+//     "value": "#268E7D",
+//     "description": "accent 300",
+//     "type": "color"
+//   },
+//   "green-200": {
+//     "value": "#39B29D",
+//     "description": "Main color",
+//     "type": "color"
+//   },
+//   "green-100": {
+//     "value": "#39C7AF",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "blue-400": {
+//     "value": "#115DA3",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "blue-300": {
+//     "value": "#146DBF",
+//     "description": "",
+//     "type": "color"
+//   },
+//   "blue-200": {
+//     "value": "#1678D1",
+//     "description": "",
+//     "type": "color"
+//   }
+// },
+// "theme": {
+//   "color-primary": {
+//     "100": {
+//       "value": "{color.violet-100}",
+//       "type": "color"
+//     },
+//     "300": {
+//       "value": "{color.violet-300}",
+//       "type": "color"
+//     },
+//     "400": {
+//       "value": "{color.violet-400}",
+//       "type": "color"
+//     },
+//     "main": {
+//       "value": "{color.violet-200}",
+//       "type": "color"
+//     }
+//   },
+//   "color-secondary": {
+//     "100": {
+//       "value": "{color.orange-100}",
+//       "type": "color"
+//     },
+//     "300": {
+//       "value": "{color.orange-300}",
+//       "type": "color"
+//     },
+//     "400": {
+//       "value": "{color.orange-400}",
+//       "type": "color"
+//     },
+//     "main": {
+//       "value": "{color.orange-200}",
+//       "type": "color"
+//     }
+//   },
+//   "color-neutral": {
+//     "25": {
+//       "value": "{color.grey-25}",
+//       "type": "color"
+//     },
+//     "50": {
+//       "value": "{color.grey-50}",
+//       "type": "color"
+//     },
+//     "100": {
+//       "value": "{color.grey-100}",
+//       "type": "color"
+//     },
+//     "200": {
+//       "value": "{color.grey-200}",
+//       "type": "color"
+//     },
+//     "300": {
+//       "value": "{color.grey-300}",
+//       "type": "color"
+//     },
+//     "400": {
+//       "value": "{color.grey-400}",
+//       "type": "color"
+//     },
+//     "black": {
+//       "value": "{color.black}",
+//       "type": "color"
+//     },
+//     "white": {
+//       "value": "{color.white}",
+//       "type": "color"
+//     }
+//   },
+//   "color-negative": {
+//     "100": {
+//       "value": "{color.red-100}",
+//       "type": "color"
+//     },
+//     "300": {
+//       "value": "{color.red-300}",
+//       "type": "color"
+//     },
+//     "400": {
+//       "value": "{color.red-400}",
+//       "type": "color"
+//     },
+//     "main": {
+//       "value": "{color.red-200}",
+//       "type": "color"
+//     }
+//   },
+//   "color-warning": {
+//     "100": {
+//       "value": "{color.yellow-100}",
+//       "type": "color"
+//     },
+//     "300": {
+//       "value": "{color.yellow-300}",
+//       "type": "color"
+//     },
+//     "400": {
+//       "value": "{color.yellow-400}",
+//       "type": "color"
+//     },
+//     "main": {
+//       "value": "{color.yellow-200}",
+//       "type": "color"
+//     }
+//   },
+//   "color-accent": {
+//     "100": {
+//       "value": "{color.green-100}",
+//       "type": "color"
+//     },
+//     "300": {
+//       "value": "{color.green-300}",
+//       "type": "color"
+//     },
+//     "400": {
+//       "value": "{color.green-400}",
+//       "type": "color"
+//     },
+//     "main": {
+//       "value": "{color.green-200}",
+//       "type": "color"
+//     }
+//   },
+//   "color-informative": {
+//     "100": {
+//       "value": "#1B92FF",
+//       "type": "color"
+//     },
+//     "300": {
+//       "value": "{color.blue-300}",
+//       "type": "color"
+//     },
+//     "400": {
+//       "value": "{color.blue-400}",
+//       "type": "color"
+//     },
+//     "main": {
+//       "value": "{color.blue-200}",
+//       "type": "color"
+//     }
+//   }
+// },
