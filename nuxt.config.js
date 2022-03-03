97d66c4f398396d24e7a3cb2c7a7486c3c1edace
MarcusCunhaPessoalM1Air <marcus.cunha@engdb.com.br>
2022-03-03 14:21:28 -0400

    instalando manualmente plugin do oruga com as 2 chamadas de css a ser sobrescrita

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~plugins/oruga.js' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -41,6 +41,6 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   oruga: {
-    includeCss: false
+    includeCss: false,
   },
 }
